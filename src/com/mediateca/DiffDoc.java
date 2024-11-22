a811966757229e77f2326b5bed2445a70a90174a
raque1997 <raquelc.20161@gmail.com>
2024-11-21 20:20:33 -0600

    Initial commit with project

---
+++ src/com/mediateca/DiffDoc.java
@@ -0,0 +1,90 @@
+package com.mediateca;
+
+/**
+ * Clase DiffDoc representa documentos diferentes en la mediateca.
+ */
+public class DiffDoc {
+    // Atributos de ejemplo
+    private String codigo;
+    private String titulo;
+    private String tipoDocumento; // Tipo: mapa, manual, folleto, etc.
+    private String descripcion;   // Breve descripción del documento
+    private int anioPublicacion;
+    private int unidadesDisponibles;
+
+    // Constructor vacío
+    public DiffDoc() {
+    }
+
+    // Constructor completo
+    public DiffDoc(String codigo, String titulo, String tipoDocumento, String descripcion, int anioPublicacion, int unidadesDisponibles) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.tipoDocumento = tipoDocumento;
+        this.descripcion = descripcion;
+        this.anioPublicacion = anioPublicacion;
+        this.unidadesDisponibles = unidadesDisponibles;
+    }
+
+    // Getters y Setters
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public int getUnidadesDisponibles() {
+        return unidadesDisponibles;
+    }
+
+    public void setUnidadesDisponibles(int unidadesDisponibles) {
+        this.unidadesDisponibles = unidadesDisponibles;
+    }
+
+    // Método para mostrar información del documento
+    @Override
+    public String toString() {
+        return "DiffDoc{" +
+                "codigo='" + codigo + '\'' +
+                ", titulo='" + titulo + '\'' +
+                ", tipoDocumento='" + tipoDocumento + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", anioPublicacion=" + anioPublicacion +
+                ", unidadesDisponibles=" + unidadesDisponibles +
+                '}';
+    }
+}
